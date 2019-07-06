b176e542bd94eeb46542c82d46c4e0a5cd618590
Pavlin Georgiev <pgeorgie@redhat.com>
2019-07-06 21:12:42 +0200

    Add scripts for installing RHEL on VM via kickstart

---
+++ rhel-8.1-vm-install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Install nightly build of specific version of RHEL 8.1.
+virt-install \
+--name RHEL-8.1-Latest \
+--memory 2048 \
+--vcpus 2 \
+--disk size=20 \
+--network default \
+--location http://download.eng.rdu2.redhat.com/rel-eng/latest-RHEL-8.1/compose/BaseOS/x86_64/os/ \
+--os-variant rhel8.0 \
+--initrd-inject /home/pgeorgie/VM/kickstart/rhel8/kickstart.cfg \
+--extra-args="ks=file:/kickstart.cfg console=tty0 console=ttyS0,115200n8 biosdevname=0 net.ifnames=0"
