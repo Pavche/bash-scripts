8d96650756994c2e4dc0a0aff3e0f24b74581d87
Pavlin Georgiev <pgeorgie@redhat.com>
2019-07-29 14:06:58 +0200

    Update scripts for installing VM from kickstart

--- rhel-7.7-vm-install.sh
+++ rhel-7.7-vm-install.sh
@@ -2,12 +2,12 @@
 
 # Install nightly build of specific version of RHEL 7.7.
 virt-install \
---name RHEL-7.7-Workstation \
+--name RHEL-7.7-Workstation-Latest \
 --memory 2048 \
 --vcpus 2 \
---disk size=20 \
+--disk pool=VM,size=20 \
 --network default \
 --location http://10.10.160.20/rel-eng/latest-RHEL-7.7/compose/Workstation/x86_64/os/ \
 --os-variant rhel7 \
---initrd-inject /home/pgeorgie/VM/kickstart/rhel-7.7/kickstart.cfg \
+--initrd-inject /home/pgeorgie/VM/kickstart/RHEL7/kickstart.cfg \
 --extra-args="ks=file:/kickstart.cfg console=tty0 console=ttyS0,115200n8"
--- rhel-8.1-vm-kickstart-gen.sh
+++ rhel-8.1-vm-kickstart-gen.sh
@@ -5,7 +5,7 @@ virt-install \
 --name RHEL-8.1-Latest-nightly \
 --memory 2048 \
 --vcpus 1 \
---disk size=10 \
+--disk pool=VM,size=10 \
 --network default \
 --location http://download.eng.rdu2.redhat.com/rel-eng/latest-RHEL-8.1/compose/BaseOS/x86_64/os/ \
 --os-variant rhel8.0 \
