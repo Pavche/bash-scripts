d9847997ff7ffa56e110a2ed4a3c9421ce74b2e5
Pavlin Georgiev <pgeorgie@redhat.com>
2019-09-02 09:56:35 +0200

    Add prerequisites to debugging script

--- debug-wpa-supplicant.sh
+++ debug-wpa-supplicant.sh
@@ -10,6 +10,25 @@
 # OTHER_ARGS="-P /var/run/wpa_supplicant.pid -dddK"
 # Restart WPA suplicant's service.
 
+# Check prerequisites
+if ! rpm --quiet -q wpa_supplicant; then
+  echo "WPA supplicant is not installed" >&2
+  sleep 3
+  exit 1
+fi
+
+if ! [ -f /etc/sysconfig/wpa_supplicant ]; then
+  echo "The configuration file of WPA supplicant missing or not recognized" >&2
+  sleep 3 
+  exit 2
+fi
+
+if ! [ -w /etc/sysconfig/wpa_supplicant ]; then
+  echo "The configuration file of WPA supplicant cannot be edited." >&2
+  sleep 3 
+  exit 3
+fi
+
 sed -i '/OTHER_ARGS/d'  /etc/sysconfig/wpa_supplicant
 echo 'OTHER_ARGS="-P /var/run/wpa_supplicant.pid -dddK"' >> /etc/sysconfig/wpa_supplicant
 systemctl restart wpa_supplicant
